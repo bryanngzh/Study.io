fdde36c0c37a3f5955bbb0e968d69ffdbe71296f
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-05-27 23:50:50 +0800

    #22 fix ui for landing and registration page

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,13 +1,31 @@
 import axios from "axios"
 import { useState, useContext, useEffect } from "react"
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { AuthContext } from "../helpers/AuthContext";
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 const Registration = () => {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false);
 
     const { authState } = useContext(AuthContext);
 
@@ -26,49 +44,139 @@ const Registration = () => {
             if (response.data.error) {
                 alert(response.data.error)
             } else {
-                navigate("/login")
+                navigate("/")
             }
         })
     }
 
     return (
-        <div>
-             <h1 className="large text-primary">Sign Up</h1>
-                <p className="lead">
-                    <i className="fas fa-user"></i> Create Your Account
-                </p>
-            <form className="form" onSubmit={registerUser}>
-              <div className="form-group">
-                <input 
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    type="text"
-                    placeholder="Username" 
-                />
-              </div>
-              <div className="form-group">
-                <input 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    placeholder="Email" 
-                />
-              </div>
-              <div className="form-group">
-                <input 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="password"
-                    placeholder="Password" 
+        <Flex
+          minH={'60vh'}
+          align={'center'}
+          justify={'center'}
+          bg={useColorModeValue('white.100', 'black.900')}>
+          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+            <Stack align={'center'}>
+              <Heading fontSize={'4xl'} textAlign={'center'}>
+                Sign up
+              </Heading>
+              <Text fontSize={'lg'} color={'gray.600'}>
+                to enjoy all of our cool features ✌️
+              </Text>
+            </Stack>
+            <Box
+              rounded={'lg'}
+              bg={useColorModeValue('white', 'gray.700')}
+              boxShadow={'lg'}
+              p={8}>
+              <Stack spacing={4}>
+                <HStack>
+                  <Box>
+                    <FormControl id="firstName" isRequired>
+                      <FormLabel>Username</FormLabel>
+                      <Input 
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                    </FormControl>
+                  </Box>
+                  {/* <Box>
+                    <FormControl id="lastName">
+                      <FormLabel>Last Name</FormLabel>
+                      <Input type="text" />
+                    </FormControl>
+                  </Box> */}
+                </HStack>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                    <Input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
-                </div>
-                <input type="submit" className="btn btn-primary" value="Register" />      
-            </form>
-            <p className="my-1">
-              Already have an account? <Link to ="/login">Sign In</Link>
-            </p>
-        </div>
-    )
+                </FormControl>
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      type={showPassword ? 'text' : 'password'} 
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}         
+                    />
+                    <InputRightElement h={'full'}>
+                      <Button
+                        variant={'ghost'}
+                        onClick={() =>
+                          setShowPassword((showPassword) => !showPassword)
+                        }>
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    loadingText="Submitting"
+                    onClick={registerUser}
+                    size="lg"
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}>
+                    Sign up
+                  </Button>
+                </Stack>
+                <Stack pt={6}>
+                  <Text align={'center'}>
+                    Already a user? <Link href="/" color={'blue.400'}>Login</Link>
+                  </Text>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Flex>
+      );
+
+    // return (
+    //     <div>
+    //          <h1 className="large text-primary">Sign Up</h1>
+    //             <p className="lead">
+    //                 <i className="fas fa-user"></i> Create Your Account
+    //             </p>
+    //         <form className="form" onSubmit={registerUser}>
+    //           <div className="form-group">
+    //             <input 
+    //                 value={username}
+    //                 onChange={(e) => setUsername(e.target.value)}
+    //                 type="text"
+    //                 placeholder="Username" 
+    //             />
+    //           </div>
+    //           <div className="form-group">
+    //             <input 
+    //                 value={email}
+    //                 onChange={(e) => setEmail(e.target.value)}
+    //                 type="email"
+    //                 placeholder="Email" 
+    //             />
+    //           </div>
+    //           <div className="form-group">
+    //             <input 
+    //                 value={password}
+    //                 onChange={(e) => setPassword(e.target.value)}
+    //                 type="password"
+    //                 placeholder="Password" 
+    //                 />
+    //             </div>
+    //             <input type="submit" className="btn btn-primary" value="Register" />      
+    //         </form>
+    //         <p className="my-1">
+    //           Already have an account? <Link to ="/login">Sign In</Link>
+    //         </p>
+    //     </div>
+    // )
 }
 
 export default Registration
