4839bfe9e1d0b70d9591491cc2d1112c74f24e6e
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-07-23 01:00:52 +0800

    Improve Note Editor function and UI

--- client/src/components/NoteEditor.jsx
+++ client/src/components/NoteEditor.jsx
@@ -1,143 +1,44 @@
-import "../styles/noteEditor.css"
+import "../styles/noteEditor.scss"
 import { BubbleMenu, useEditor, EditorContent, FloatingMenu } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
 import Highlight from '@tiptap/extension-highlight'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Blockquote from '@tiptap/extension-blockquote'
+import Text from '@tiptap/extension-text'
+import TextStyle from '@tiptap/extension-text-style'
+import FontFamily from '@tiptap/extension-font-family'
+import CharacterCount from '@tiptap/extension-character-count'
+import { Color } from '@tiptap/extension-color'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import { lowlight } from 'lowlight'
 import {
   Button,
-  Text,
+  Text as Text1,
   Box,
-  Tooltip
+  Tooltip,
+  useColorModeValue,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
 } from "@chakra-ui/react"
+import { ChevronDownIcon } from "@chakra-ui/icons"
 import { FaFilePdf } from "react-icons/fa"
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import html2pdf from 'html2pdf.js'
 
-const MenuBar = ({ editor }) => {
-  if (!editor) {
-    return null
-  }
-
-  return (
-    <>
-      <Button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active' : ''}
-      >
-        bold
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active' : ''}
-      >
-        italic
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive('strike') ? 'is-active' : ''}
-      >
-        strike
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        className={editor.isActive('code') ? 'is-active' : ''}
-      >
-        code
-      </Button>
-      <Button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear marks
-      </Button>
-      <Button onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear nodes
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? 'is-active' : ''}
-      >
-        paragraph
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
-      >
-        h1
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
-      >
-        h2
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-      >
-        h3
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
-      >
-        h4
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
-      >
-        h5
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
-      >
-        h6
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        bullet list
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        ordered list
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
-      >
-        code block
-      </Button>
-      <Button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? 'is-active' : ''}
-      >
-        blockquote
-      </Button>
-      <Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
-      </Button>
-      <Button onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
-      </Button>
-      <Button onClick={() => editor.chain().focus().undo().run()}>
-        undo
-      </Button>
-      <Button onClick={() => editor.chain().focus().redo().run()}>
-        redo
-      </Button>
-    </>
-  )
-}
-
-
 //edit the tool bar to make it look nicer
 const NoteEditor = (props) => {
   const [note, setNote] = useState()
   const [active, setActive] = useState(true)
+  const buttonColor = "telegram"
 
   useEffect(() => {
     if (active) {
@@ -161,13 +62,30 @@ const NoteEditor = (props) => {
 
   const editor = useEditor({
     extensions: [
+      CodeBlockLowlight.configure({
+        lowlight,
+        // languageClassPrefix: 'language-javascript',
+      }),
       StarterKit.configure({
         heading: {
           levels: [1, 2],
         },
+        codeBlock: false,
       }),
       Underline,
       Highlight,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Blockquote,
+      Text,
+      TextStyle,
+      FontFamily,
+      CharacterCount.configure({
+        limit: null,
+      }),
+      Color,
     ],
     content: '',
     onUpdate: ({ editor }) => {
@@ -216,44 +134,188 @@ const NoteEditor = (props) => {
     html2pdf().set(opt).from(html).save();
   }
 
+  if (!editor) {
+    return null
+  }
+
   return (
     <div title="noteeditor">
       {editor && <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-        <Button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive('bold') ? 'is-active' : ''}
-          size="xs"
-        >
-          B
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive('italic') ? 'is-active' : ''}
-          size="xs"
-        >
-          <Text as='i'>i</Text>
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleUnderline().run()}
-          className={editor.isActive('underline') ? 'is-active' : ''}
-          size="xs"
-        >
-          <Text as='u'>U</Text>
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleHighlight().run()}
-          className={editor.isActive('highlight') ? 'is-active' : ''}
-          size="xs"
-        >
-          <Text>H</Text>
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          className={editor.isActive('strike') ? 'is-active' : ''}
-          size="xs"
-        >
-           <Text as='s'>S</Text>
-        </Button>
+        <Tooltip label="Bold" aria-label='A tooltip' placement='top'>
+          <Button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive('bold') ? 'is-active' : ''}
+            size="xs"
+            colorScheme={buttonColor}
+          >
+            B
+          </Button>
+        </Tooltip>
+        <Tooltip label="italicise" aria-label='A tooltip' placement='top'>
+          <Button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'is-active' : ''}
+            size="xs"
+            colorScheme={buttonColor}
+          >
+            <Text1 as='i'>i</Text1>
+          </Button>
+        </Tooltip>
+        <Tooltip label="Underline" aria-label='A tooltip' placement='top'>
+          <Button
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            className={editor.isActive('underline') ? 'is-active' : ''}
+            size="xs"
+            colorScheme={buttonColor}
+          >
+            <Text1 as='u'>U</Text1>
+          </Button>
+        </Tooltip>
+        <Tooltip label="Highlight" aria-label='A tooltip' placement='top'>
+          <Button
+            onClick={() => editor.chain().focus().toggleHighlight().run()}
+            className={editor.isActive('highlight') ? 'is-active' : ''}
+            size="xs"
+            colorScheme={buttonColor}
+          >
+            <Text1>H</Text1>
+          </Button>
+        </Tooltip>
+        <Tooltip label="Strikethrough" aria-label='A tooltip' placement='top'>
+          <Button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive('strike') ? 'is-active' : ''}
+            size="xs"
+            colorScheme={buttonColor}
+          >
+            <Text1 as='s'>S</Text1>
+          </Button>
+        </Tooltip>
+        
+          <Menu closeOnSelect={false}>
+            <Tooltip label="Font" aria-label='A tooltip' placement='top'>
+              <MenuButton as={Button} size="xs" colorScheme={buttonColor} rightIcon={<ChevronDownIcon />}>
+                Font
+              </MenuButton>
+            </Tooltip>
+            <MenuList>
+                <MenuOptionGroup defaultValue='none' type='radio'>
+                  <MenuItemOption
+                    value='none'
+                    onClick={() => editor.chain().focus().unsetFontFamily().run()}>
+                    <text>Default</text>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='inter'
+                    onClick={() => editor.chain().focus().setFontFamily('Inter').run()}
+                    className={editor.isActive('textStyle', { fontFamily: 'Inter' }) ? 'is-active' : ''}>
+                    <p><span style={{ fontFamily: "Inter" }}>Inter</span></p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='comic'
+                    onClick={() => editor.chain().focus().setFontFamily('Comic Sans MS, Comic Sans').run()}
+                    className={
+                      editor.isActive('textStyle', { fontFamily: 'Comic Sans MS, Comic Sans' })
+                        ? 'is-active'
+                        : ''
+                    }>
+                    <p><span style={{ fontFamily: "Comic Sans MS, Comic Sans" }}>Comic Sans</span></p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='serif'
+                    onClick={() => editor.chain().focus().setFontFamily('serif').run()}
+                    className={editor.isActive('textStyle', { fontFamily: 'serif' }) ? 'is-active' : ''}>
+                    <p><span style={{ fontFamily: "serif" }}>serif</span></p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='mono'
+                    onClick={() => editor.chain().focus().setFontFamily('monospace').run()}
+                    className={editor.isActive('textStyle', { fontFamily: 'monospace' }) ? 'is-active' : ''}>
+                    <p><span style={{ fontFamily: "monospace" }}>monospace</span></p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='cursive'
+                    onClick={() => editor.chain().focus().setFontFamily('cursive').run()}
+                    className={editor.isActive('textStyle', { fontFamily: 'cursive' }) ? 'is-active' : ''}>
+                    <p><span style={{ fontFamily: "cursive" }}>cursive</span></p>
+                  </MenuItemOption>
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+        
+        <Menu closeOnSelect={false}>
+          <Tooltip label="Text Colour" aria-label='A tooltip' placement='top'>
+            <MenuButton as={Button} size="xs" colorScheme={buttonColor} rightIcon={<ChevronDownIcon />}>
+              A
+            </MenuButton>
+          </Tooltip>
+            <MenuList>
+                
+                  <MenuItemOption>
+                  <input
+                    type="color"
+                    onInput={event => editor.chain().focus().setColor(event.target.value).run()}
+                    value={editor.getAttributes('textStyle').color}
+                  />
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='black'
+                    onClick={() => editor.chain().focus().unsetColor().run()}
+                  >
+                    <p><span style={{ fontWeight: 'bold'}}>A</span> Default</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='purple'
+                    onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+                    className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#958DF1', fontWeight: 'bold'}}>A</span> Purple</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='red'
+                    onClick={() => editor.chain().focus().setColor('#F98181').run()}
+                    className={editor.isActive('textStyle', { color: '#F98181' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#F98181', fontWeight: 'bold' }}>A</span> Red</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='orange'
+                    onClick={() => editor.chain().focus().setColor('#FBBC88').run()}
+                    className={editor.isActive('textStyle', { color: '#FBBC88' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#FBBC88', fontWeight: 'bold' }}>A</span> Orange</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='yellow'
+                    onClick={() => editor.chain().focus().setColor('#FAF594').run()}
+                    className={editor.isActive('textStyle', { color: '#FAF594' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#FAF594', fontWeight: 'bold' }}>A</span> Yellow</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='blue'
+                    onClick={() => editor.chain().focus().setColor('#70CFF8').run()}
+                    className={editor.isActive('textStyle', { color: '#70CFF8' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#70CFF8', fontWeight: 'bold' }}>A</span> Blue</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='teal'
+                    onClick={() => editor.chain().focus().setColor('#94FADB').run()}
+                    className={editor.isActive('textStyle', { color: '#94FADB' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#94FADB', fontWeight: 'bold' }}>A</span> Teal</p>
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value='green'
+                    onClick={() => editor.chain().focus().setColor('#B9F18D').run()}
+                    className={editor.isActive('textStyle', { color: '#B9F18D' }) ? 'is-active' : ''}
+                  >
+                    <p><span style={{ color: '#B9F18D', fontWeight: 'bold' }}>A</span> Green</p>
+                  </MenuItemOption>
+              
+            </MenuList>
+          </Menu> 
       </BubbleMenu>}
 
       {editor && <FloatingMenu className="floating-menu" tippyOptions={{ duration: 100 }} editor={editor}>
@@ -276,18 +338,37 @@ const NoteEditor = (props) => {
           className={editor.isActive('bulletList') ? 'is-active' : ''}
           size="xs"
         >
-          Bullet List
+          BulletList
         </Button>
         <Button
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
           className={editor.isActive('codeBlock') ? 'is-active' : ''}
           size="xs"
         >
-          toggleCodeBlock
+          CodeBlock
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleTaskList().run()}
+          className={editor.isActive('taskList') ? 'is-active' : ''}
+          size="xs"
+        >
+          TaskList
+        </Button>
+        <Button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive('blockquote') ? 'is-active' : ''}
+          size="xs"
+        >
+          Blockquote
         </Button>
       </FloatingMenu>}
       {/* <MenuBar editor={editor} /> */}
       <EditorContent editor={editor} />
+      <div className="character-count">
+        {editor.storage.characterCount.characters()} characters
+        <br />
+        {editor.storage.characterCount.words()} words
+      </div>
       {localStorage.getItem("chakra-ui-color-mode") === "light" ?
         <Tooltip label="Download as PDF">
           <Button colorScheme="orange" variant='outline' onClick={() => exportHtml()}><FaFilePdf /></Button>
