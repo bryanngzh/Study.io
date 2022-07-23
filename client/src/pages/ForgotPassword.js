b7c61db5587cf49c3e74d954e53e06ea7a9e3be8
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2022-07-23 23:49:22 +0800

    fixed minor bug

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -43,7 +43,7 @@ const ForgotPassword = (props) => {
 
     const sendEmail = (event) => {
 
-      axios.post("/api/auth/requestPasswordReset", {email: email, redirectUrl: "http://localhost:3000"}).then((response) => {
+      axios.post("/api/auth/requestPasswordReset", {email: email, redirectUrl: "https://study-io.herokuapp.com/"}).then((response) => {
         console.log(response.data)
         if (response.data.error) {
           alert(response.data.error)
